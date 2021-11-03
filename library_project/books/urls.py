ecf0a8f8c0707743099fc88c167017f9755f71d8
greatlaki <greatlaki911@gmail.com>
2021-11-03 22:51:08 +0300

    Routing. Path - "addbook/"

--- library_project/books/urls.py
+++ library_project/books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import BookListView
+from .views import *
 
 
 urlpatterns = [
     path('', BookListView.as_view(), name='home'),
+    path('addbook/', book_upload, name='add_book'),
 ]
