4e711b1fd753c27ab1045e37333b16bcddfe15e9
greatlaki <greatlaki911@gmail.com>
2021-11-04 00:34:56 +0300

    Added urls

--- library_project/books/urls.py
+++ library_project/books/urls.py
@@ -6,4 +6,8 @@ from .views import *
 urlpatterns = [
     path('', BookListView.as_view(), name='home'),
     path('addbook/', book_upload, name='add_book'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('logout/', logout_user, name='logout'),
+
 ]
