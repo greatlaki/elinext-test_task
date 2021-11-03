66109c0b7d98fd03be14f561e3cc588aea5bb0e3
greatlaki <greatlaki911@gmail.com>
2021-11-04 00:35:44 +0300

    Added registration and login forms

--- library_project/books/forms.py
+++ library_project/books/forms.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 
 
 class RegisterUserForm(UserCreationForm):
-    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    username = forms.CharField(label='Логин')
+    password1 = forms.CharField(label='Пароль')
+    password2 = forms.CharField(label='Повтор пароля')
 
     class Meta:
         model = User
