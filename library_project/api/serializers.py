196c45cf81de7e0ac39d6ac19559c6efdce124a2
greatlaki <greatlaki911@gmail.com>
2021-11-07 11:28:33 +0300

    Made a file update "serializers.py"

--- library_project/api/serializers.py
+++ library_project/api/serializers.py
@@ -7,6 +7,12 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from books.models import Book
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'username', 'email', 'first_name', 'is_active']
+
+
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
@@ -45,12 +51,3 @@ class RegisterSerializer(serializers.ModelSerializer):
         return user
 
 
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-
-    @classmethod
-    def get_token(cls, user):
-        token = super(MyTokenObtainPairSerializer, cls).get_token(user)
-
-        # Add custom claims
-        token['username'] = user.username
-        return token
