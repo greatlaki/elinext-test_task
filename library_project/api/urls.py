495f1a7eb14e137d08a3e60843017675fef08d87
greatlaki <greatlaki911@gmail.com>
2021-11-07 11:42:23 +0300

    Updated the "urls" file

--- library_project/api/urls.py
+++ library_project/api/urls.py
@@ -1,15 +1,15 @@
-from django.urls import path
-from rest_framework.authtoken import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from .views import *
 
 
 urlpatterns = [
     path('', BookApiView.as_view()),
-    path('login/', LoginUser.as_view()),
+    path('user/', user, name='user'),
+    path('login/', login_view, name='login'),
+    path('token-refresh/', refresh_token_view),
     path('register/', RegisterUser.as_view(), name='auth_register'),
     path('logout/', user_logout),
-    path('user/', UserView.as_view()),
-    path('api-token-auth/', views.obtain_auth_token),
 ]
 
