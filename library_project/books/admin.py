84c715f1a809f7056b5707a99cbe296f51586c5f
greatlaki <greatlaki911@gmail.com>
2021-11-07 18:53:34 +0300

    Added sorting, filtering and a unique option

--- library_project/books/admin.py
+++ library_project/books/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'written_year', 'birth_year')
+    list_display_links = ('id', 'title', 'author', 'written_year', 'birth_year')
+    search_fields = ('title', 'author', 'written_year', 'birth_year')
+
+
+admin.site.register(Book, BookAdmin)
 
--- library_project/books/models.py
+++ library_project/books/models.py
@@ -5,21 +5,29 @@ from django.dispatch import receiver
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=250)
-    author = models.CharField(max_length=100)
-    written_year = models.IntegerField()
-    birth_year = models.IntegerField()
+    title = models.CharField(max_length=250, verbose_name='Book title')
+    author = models.CharField(max_length=100, verbose_name='Author')
+    written_year = models.IntegerField(verbose_name='Book written year')
+    birth_year = models.IntegerField(verbose_name='Author birth year')
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+        ordering = ['title', 'author']
+        unique_together = (
+            ('title', 'author'),
+        )
+
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.TextField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=30, blank=True)
-    phone_number = models.DateField(null=True, blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Username')
+    first_name = models.TextField(max_length=30, blank=True, verbose_name='First name')
+    last_name = models.CharField(max_length=30, blank=True, verbose_name='Last name')
+    phone_number = models.DateField(null=True, blank=True, verbose_name='Phone number')
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name='Photo')
 
 
 @receiver(post_save, sender=User)
