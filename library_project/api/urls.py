0689ba619d56e23661429147163868a5380bf669
greatlaki <greatlaki911@gmail.com>
2021-11-09 17:01:48 +0300

    Some changes

--- library_project/api/urls.py
+++ library_project/api/urls.py
@@ -8,7 +8,7 @@ from .views import *
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
-    path('logout/', LogoutAPIView.as_view(), name='logout')
+    path('logout/', LogoutAPIView.as_view(), name='logout'),
     path('profile-user/', ProfileListAPIView.as_view(), name='profile-user'),
     path('profile-user/<int:id>', ProfileDetailAPIView.as_view(), name='profile-user'),
     path('username-verify/', VerifyUsername.as_view(), name='username-verify'),
